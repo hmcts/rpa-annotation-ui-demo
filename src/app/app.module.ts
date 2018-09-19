a5f8cc108680fe24d8203aa22b958ab67b87cf62
Phillip Denness <dennessphillip@gmail.com>
2018-09-19 14:07:53 +0100

    Basic test bed for PDF lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { AnnotationUiLibModule, ViewerComponent} from 'hmcts-annotation-ui-lib';
+import { Routes, RouterModule } from '@angular/router';
+import { LineComponent } from './line/line.component';
+
+const appRoutes: Routes = [
+  { path: '',  component: ViewerComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AnnotationUiLibModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
